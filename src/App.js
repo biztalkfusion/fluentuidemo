3fbb7e40b15f2b95204cc2b1512ed915249e0ba4
Ganeshan Venkataraman <ganeshanv@msn.com>
2020-09-10 15:59:26 -0400

    Imports

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Greeting from "./WelcomeTS";
 
-function App() {
+function MyStuff(props) {
+  return (<>This is {props.name} writing something</>)
+}
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+export default function App() {
+  const title = 'Whatever title'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello StackBlitz!</h1>
+      <Greeting name="God Almighty"></Greeting>
+      <Welcome name="sheshadri"></Welcome>
+      <MyStuff name="Ganeshan"/>
+      <MyStuff name="Guru"/>
+      <h2 id="myHeading">This is a heading 2 {title}</h2>
+      <p>Start editing to see some magic happen :)</p>
+    </>
   );
 }
-
-export default App;
